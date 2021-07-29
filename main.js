c59c00bde228583df913fffa48fa3e8eb1aa7cd9
sherkaicode <fljumawan@gmail.com>
2021-07-29 17:50:52 +0800

    main file, images produced and package-lock

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+require('dotenv').config();
+var client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]})
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord)
+
+})
+
+client.login(process.env.TOKEN)
