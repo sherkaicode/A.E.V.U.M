9cff92fd48a8fc278dccaf05a2bc7e2ed737ac93
sherkaicode <fljumawan@gmail.com>
2021-08-02 14:24:07 +0800

    added caption

--- commands/post.js
+++ commands/post.js
@@ -1,21 +1,70 @@
 const fs = require('fs')
 const FB = require('fb');
+var mods = require("../Modules.js")
 require('dotenv').config();
 module.exports = {
     name: 'post',
     description: "Pings Latency",
     async execute(client, message, args, Discord) {
-        var nam = args[0];
-        console.log('SS')
-        const ACCESS_TOKEN = process.env.FBTOKEN;
-        FB.setAccessToken(ACCESS_TOKEN);
-        FB.api('AegisCodez/photos', 'post', { source: fs.createReadStream(`${nam}.png`), caption: 'Swag' }, function (res) {
-            if (!res || res.error) {
-                console.log(!res ? 'error occurred' : res.error);
-                return;
+        var today = new Date();
+        const daysInMonth = mods.daysInMonth(today.getFullYear(), today.getMonth() + 1)
+        var week = today.getDay(); 
+        var tod = today.getDate();
+        var weeknum1 = -1 * ((week - (tod -1)) % 7);
+        var dates;
+        var weekends = mods.getweekend(daysInMonth, weeknum1, tod)
+        if(weekends.length > 2) {
+            dates = `${weekends.length}`
+        }
+        else {
+            for(c = 0; c<weekends.length; c++) {
+                dates = dates + `${weekends[c]}`
             }
-            console.log('Post Id: ' + res.post_id);
-        });
+        }
+        const ACCESS_TOKEN = process.env.FBTOKEN;
+        var run = true;
+        var nam = args[0];
+        var captionsmall = `Ka-Bitoon❗️ ${dates} Weekend slot are still available
+        \n❗️ Book Na ❗️ 
+        \n🟡To secure your desired date, be sure to book early and pay at least the minimum downpayment (500 pesos). The first client to pay the downpayment keeps the date. 
+        \nSmall Floating Cottage Pics 
+        \n👉https://photos.app.goo.gl/ZDqvAd9nZnMVgD6Q7
+        \nFor reservations, you may call/text:0997 873 3341 
+        \nSee you soon mga Ka-Bitoon ✨`
+        var captionbig = `Ka-Bitoon❗️ ${dates} Weekend slot are still available
+        \n❗️ Book Na ❗️ 
+        \n🟡To secure your desired date, be sure to book early and pay at least the minimum downpayment (1000 pesos). The first client to pay the downpayment keeps the date. 
+        \nBig Floating Cottage Pics 
+        \n👉https://photos.app.goo.gl/cbNqhKKj4pgKAwcq5
+        \nFor reservations, you may call/text:0997 873 3341 
+        \nSee you soon mga Ka-Bitoon ✨`
+        var captio
+        try {
+            if (nam == 'big') {
+                captio = captionbig
+            }
+            else if (nam == 'small') {
+                captio = captionsmall
+            }
+            else {
+                run = false
+            }
+        }
+        catch {
+            run = false
+        }
+  
+        if (run) {
+            FB.setAccessToken(ACCESS_TOKEN);
+            FB.api('AegisCodez/photos', 'post', { source: fs.createReadStream(`${nam}.png`), caption: captio }, function (res) {
+                if (!res || res.error) {
+                    console.log(!res ? 'error occurred' : res.error);
+                    return;
+                }
+                console.log('Post Id: ' + res.post_id);
+                message.channel.send('Posted Successfully')
+            });
+        }
 
     }
 }
